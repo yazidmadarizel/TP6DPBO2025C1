f0d919705c604ae0d772b72e69b8f58d00ed2da9
yazidmadarizel <69192961+yazidmadarizel@users.noreply.github.com>
2025-04-13 17:42:19 +0700

    Update App.java

--- FlappyBird/src/App.java
+++ FlappyBird/src/App.java
@@ -1,7 +1,21 @@
-import javax.swing.SwingUtilities;
+import javax.swing.*;
 
 public class App {
+    public App() {
+        JFrame frame = new JFrame("Flappy Bird");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(360, 640);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+
+        FlappyBird flappyBird = new FlappyBird();
+        frame.add(flappyBird);
+        frame.pack();
+        flappyBird.requestFocus();
+        frame.setVisible(true);
+    }
+
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(MainMenu::new);
+        new MainMenu(); 
     }
 }
